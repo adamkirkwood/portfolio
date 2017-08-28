9dabc86b31387922e5ecdf7ca95bcb95eb4b6e71
Adam Kirkwood <hello@adamkirkwood.com>
2017-08-28 13:16:31 -0700

    :lipstick: Update Footer component

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,7 +1,7 @@
 export default () => (
   <footer>
-    <div className={'c-main-footer u-pv-large u-ph-medium o-grid o-grid--wide'}>
-        <div className={'o-grid__col u-1/1 u-1/4@sm'}>
+    <div className={'c-main-footer u-ph-medium u-pv-medium o-grid o-grid--wide'}>
+        <div className={'o-grid__col u-1/1 u-1/4@sm u-mv-medium'}>
           <h5 className={'o-type-small u-uppercase'}>Whois</h5>
           <p className={'u-mt-small o-type-footer'}>
             Adam Kirkwood is a Digital Product Designer from the Bay Area,
@@ -9,14 +9,14 @@ export default () => (
           </p>
         </div>
 
-        <div className={'o-grid__col u-1/1 u-2/4@sm'}>
+        <div className={'o-grid__col u-1/1 u-2/4@sm u-mv-medium'}>
           <h5 className={'o-type-small u-uppercase'}>Colophon</h5>
           <p className={'u-mt-small o-type-footer'}>
             Coded in Atom.io. Built with iotacss, React.js and Next.js.
           </p>
         </div>
 
-        <div className={'o-grid__col u-1/1 u-1/4@sm'}>
+        <div className={'o-grid__col u-1/1 u-1/4@sm u-mv-medium'}>
           <h5 className={'o-type-small u-uppercase'}>Let&apos;s Connect</h5>
 
           <div className={'o-level'}>
